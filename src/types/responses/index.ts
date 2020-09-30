9233c5b6832a802d39cb78cad6e13291870e761f
Rémi Jarasson (Ext) <remi.jarasson-ext@ledger.fr>
2020-09-30 17:09:15 +0200

    feat: add ValidatorsController with service on /validators

--- src/types/responses/index.ts
+++ src/types/responses/index.ts
@@ -29,3 +29,4 @@ export * from './SanitizedStorageItemMetadata';
 export * from './TransactionDryRun';
 export * from './TransactionMaterial';
 export * from './ValidateAddress';
+export * from './Validator';
