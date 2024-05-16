6c8ada409b7c98ab09908e59673c47c444412e43
Stéphane Prohaszka <stephane.prohaszka@ledger.fr>
2024-05-16 17:13:10 +0200

    Update polkadot sdk version to fix /validators route
    Signed-off-by: Stéphane Prohaszka <stephane.prohaszka@ledger.fr>

--- src/services/identities/IdentitiesService.ts
+++ src/services/identities/IdentitiesService.ts
@@ -92,7 +92,7 @@ export class IdentitiesService extends AbstractService {
 			uniqAddresses
 		);
 
-		const map = new Map<String, Option<Registration>>(
+		const map = new Map<String, Option<ITuple<[Registration, Option<Bytes>]>>>(
 			addresses.map((addr, index) => [addr.toString(), parentIdentities[index]])
 		);
 
